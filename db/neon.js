8553144722af9933a7fd724209238fe24eed5418
Allycathe <alonso.iturra.j@gmail.com>
2024-12-11 19:19:05 -0300

    Actualización de la BD, para más seguridad

--- db/neon.js
+++ db/neon.js
@@ -1,6 +1,7 @@
 import { neon } from '@neondatabase/serverless';
+import dotenv from 'dotenv';
 /*Acceso a la base de datos*/
-const sql = neon(
-  'postgresql://Arrie2_owner:ZSkPphwz80cg@ep-long-dream-a5gfxn72.us-east-2.aws.neon.tech/Arrie2?sslmode=require'
-);
+dotenv.config();
+
+const sql= neon(process.env.DATABASE_URL);
 export default sql;
